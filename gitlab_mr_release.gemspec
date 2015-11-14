b5c70e30417b82705fec80377bc39f7d73aee48a
sue445 <sue445@sue445.net>
2015-11-15 01:15:39 +0900

    Impl creare

--- gitlab_mr_release.gemspec
+++ gitlab_mr_release.gemspec
@@ -29,6 +29,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "dotenv"
   spec.add_dependency "gitlab"
+  spec.add_dependency "thor"
 
   spec.add_development_dependency "activesupport"
   spec.add_development_dependency "bundler", "~> 1.10"
---
+++ lib/gitlab_mr_release/cli.rb
@@ -0,0 +1,53 @@
+require "gitlab_mr_release"
+require "thor"
+require "dotenv"
+
+module GitlabMrRelease
+  class CLI < Thor
+    GITLAB_ENV_FILES = %w(.env.gitlab ~/.env.gitlab)
+
+    DEFAULT_TEMPLATE = <<-MARKDOWN
+# MergeRequests
+<% merge_requests.each do |mr| %>
+* [ ] !<%= mr.iid %> <%= mr.title %>
+<% end %>
+    MARKDOWN
+
+    def version
+      puts GitlabMrRelease::VERSION
+    end
+
+    desc "create", "Create merge requrst"
+    option :source, aliases: "-s", required: true, desc: "Source branch (e.g. develop)"
+    option :target, aliases: "-t", required: true, desc: "Target branch (e.g. master)"
+    option :title,  desc: "MergeRequest title (default. 'Release :source -> :target')"
+    def create
+      title = options[:title] || default_title
+
+      template =
+        if ENV["TEMPLATE_FILE"]
+          File.read(ENV["TEMPLATE_FILE"])
+        else
+          DEFAULT_TEMPLATE
+        end
+
+      project = GitlabMrRelease::Project.new(
+        api_endpoint:  ENV["GITLAB_API_ENDPOINT"],
+        private_token: ENV["GITLAB_API_PRIVATE_TOKEN"],
+        project_name:  ENV["GITLAB_PROJECT_NAME"],
+      )
+      project.create_merge_request(
+        source_branch: options[:source],
+        target_branch: options[:target],
+        title:         title,
+        template:      template,
+      )
+    end
+
+    private
+
+    def default_title
+      "Release #{options[:source]} -> #{options[:target]}"
+    end
+  end
+end
