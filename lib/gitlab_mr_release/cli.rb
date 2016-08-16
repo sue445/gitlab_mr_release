0e35418259e62eed7f3e801a59f413fe895992b1
sue445 <sue445@sue445.net>
2016-08-17 02:23:26 +0900

    Support default params in .env.gitlab

--- lib/gitlab_mr_release/cli.rb
+++ lib/gitlab_mr_release/cli.rb
@@ -26,8 +26,8 @@ module GitlabMrRelease
     end
 
     desc "create", "Create merge requrst"
-    option :source, aliases: "-s", required: true, desc: "Source branch (e.g. develop)"
-    option :target, aliases: "-t", required: true, desc: "Target branch (e.g. master)"
+    option :source, aliases: "-s", desc: "Source branch (e.g. develop)"
+    option :target, aliases: "-t", desc: "Target branch (e.g. master)"
     option :title,  desc: "MergeRequest title (default. 'Release :source -> :target')"
     option :labels, aliases: "-l", desc: "Labels for MR as a comma-separated list  (e.g. 'label1,label2')"
     def create
@@ -36,6 +36,9 @@ module GitlabMrRelease
       assert_env("GITLAB_API_ENDPOINT")
       assert_env("GITLAB_API_PRIVATE_TOKEN")
       assert_env("GITLAB_PROJECT_NAME")
+
+      assert_option_or_env(:source, "DEFAULT_SOURCE_BRANCH")
+      assert_option_or_env(:target, "DEFAULT_TARGET_BRANCH")
 
       template = File.read(template_file)
 
@@ -46,9 +49,9 @@ module GitlabMrRelease
       )
 
       mr = project.create_merge_request(
-        source_branch: options[:source],
-        target_branch: options[:target],
-        labels:        options[:labels],
+        source_branch: source_branch,
+        target_branch: target_branch,
+        labels:        labels,
         title:         generate_title,
         template:      template,
       )
@@ -72,7 +75,14 @@ MergeRequest is created
     private
 
     def assert_env(name)
-      raise "Error: Environment variable #{name} is required" unless ENV[name]
+      if !ENV[name] || ENV[name].empty?
+        puts "Error: Environment variable #{name} is required"
+        exit!
+      end
+    end
+
+    def assert_option_or_env(option_name, env_name)
+      option_or_env!(option_name, env_name)
     end
 
     def generate_title
@@ -80,8 +90,8 @@ MergeRequest is created
 
       generate_default_title(
         title_template: ENV["DEFAULT_TITLE"],
-        source_branch:  options[:source],
-        target_branch:  options[:target],
+        source_branch:  source_branch,
+        target_branch:  target_branch,
       )
     end
 
@@ -92,6 +102,32 @@ MergeRequest is created
 
     def template_file
       ENV["TEMPLATE_FILE"] || "#{__dir__}/../templates/gitlab_mr_release.md.erb"
+    end
+
+    def source_branch
+      option_or_env(:source, "DEFAULT_SOURCE_BRANCH")
+    end
+
+    def target_branch
+      option_or_env(:target, "DEFAULT_TARGET_BRANCH")
+    end
+
+    def labels
+      option_or_env(:labels, "DEFAULT_LABELS")
+    end
+
+    def option_or_env(option_name, env_name, default = nil)
+      return options[option_name] if options[option_name] && !options[option_name].empty?
+      return ENV[env_name] if ENV[env_name] && !ENV[env_name].empty?
+      default
+    end
+
+    def option_or_env!(option_name, env_name)
+      value = option_or_env(option_name, env_name)
+      return value if value
+
+      puts "Error: --#{option_name} or #{env_name} is either required!"
+      exit!
     end
   end
 end
