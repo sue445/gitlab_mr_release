fe782393a2fe3df4d3d581b87af95a8fe3993d4b
sue445 <sue445@sue445.net>
2020-01-01 20:38:54 +0900

    Resolved Thor warning
    ```
    [WARNING] Attempted to create command "assert_env" without usage or description. Call desc if you want this method to be available as command or declare it inside a no_commands{} block. Invoked from "/home/runner/work/gitlab_mr_release/gitlab_mr_release/vendor/bundle/ruby/2.7.0/gems/rspec-mocks-3.9.1/lib/rspec/mocks/any_instance/recorder.rb:260:in `define_method'".
    ```

--- lib/gitlab_mr_release/cli.rb
+++ lib/gitlab_mr_release/cli.rb
@@ -78,60 +78,62 @@ MergeRequest is created
 
     private
 
-    def assert_env(name)
-      if !ENV[name] || ENV[name].empty?
-        puts "Error: Environment variable #{name} is required"
+    no_commands do
+      def assert_env(name)
+        if !ENV[name] || ENV[name].empty?
+          puts "Error: Environment variable #{name} is required"
+          exit!
+        end
+      end
+
+      def assert_option_or_env(option_name, env_name)
+        option_or_env!(option_name, env_name)
+      end
+
+      def generate_title
+        return options[:title] if options[:title]
+
+        generate_default_title(
+          title_template: ENV["DEFAULT_TITLE"],
+          source_branch:  source_branch,
+          target_branch:  target_branch,
+          )
+      end
+
+      def generate_default_title(title_template:, source_branch:, target_branch:)
+        title_template ||= DEFAULT_TITLE_TEMPLATE
+        ERB.new(title_template).result(binding).strip
+      end
+
+      def template_file
+        ENV["TEMPLATE_FILE"] || "#{__dir__}/../templates/gitlab_mr_release.md.erb"
+      end
+
+      def source_branch
+        option_or_env(:source, "DEFAULT_SOURCE_BRANCH")
+      end
+
+      def target_branch
+        option_or_env(:target, "DEFAULT_TARGET_BRANCH")
+      end
+
+      def labels
+        option_or_env(:labels, "DEFAULT_LABELS")
+      end
+
+      def option_or_env(option_name, env_name, default = nil)
+        return options[option_name] if options[option_name] && !options[option_name].empty?
+        return ENV[env_name] if ENV[env_name] && !ENV[env_name].empty?
+        default
+      end
+
+      def option_or_env!(option_name, env_name)
+        value = option_or_env(option_name, env_name)
+        return value if value
+
+        puts "Error: --#{option_name} or #{env_name} is either required!"
         exit!
       end
-    end
-
-    def assert_option_or_env(option_name, env_name)
-      option_or_env!(option_name, env_name)
-    end
-
-    def generate_title
-      return options[:title] if options[:title]
-
-      generate_default_title(
-        title_template: ENV["DEFAULT_TITLE"],
-        source_branch:  source_branch,
-        target_branch:  target_branch,
-      )
-    end
-
-    def generate_default_title(title_template:, source_branch:, target_branch:)
-      title_template ||= DEFAULT_TITLE_TEMPLATE
-      ERB.new(title_template).result(binding).strip
-    end
-
-    def template_file
-      ENV["TEMPLATE_FILE"] || "#{__dir__}/../templates/gitlab_mr_release.md.erb"
-    end
-
-    def source_branch
-      option_or_env(:source, "DEFAULT_SOURCE_BRANCH")
-    end
-
-    def target_branch
-      option_or_env(:target, "DEFAULT_TARGET_BRANCH")
-    end
-
-    def labels
-      option_or_env(:labels, "DEFAULT_LABELS")
-    end
-
-    def option_or_env(option_name, env_name, default = nil)
-      return options[option_name] if options[option_name] && !options[option_name].empty?
-      return ENV[env_name] if ENV[env_name] && !ENV[env_name].empty?
-      default
-    end
-
-    def option_or_env!(option_name, env_name)
-      value = option_or_env(option_name, env_name)
-      return value if value
-
-      puts "Error: --#{option_name} or #{env_name} is either required!"
-      exit!
     end
   end
 end
