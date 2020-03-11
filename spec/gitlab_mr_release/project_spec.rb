86ec1606d63546716f376ed21e9b98362901a67c
hisanori <hisary@gmail.com>
2020-03-11 17:39:09 +0900

    add spec

--- spec/gitlab_mr_release/project_spec.rb
+++ spec/gitlab_mr_release/project_spec.rb
@@ -98,4 +98,22 @@ describe GitlabMrRelease::Project do
 
     it { should eq description }
   end
+
+  describe "#find_current_release_mr" do
+    before do
+      stub_request(:get, "#{api_endpoint}/projects/#{escaped_project_name}/merge_requests?state=opened").
+        with(headers: { "Accept" => "application/json", "Private-Token" => private_token }).
+        to_return(status: 200, body: read_stub("merge_requests_with_state_opened.json"), headers: {})
+    end
+
+    context("the release mr exists") do
+      subject { project.find_current_release_mr("develop", "master") }
+      it { should_not be_nil }
+    end
+
+    context("the release mr does not exist") do
+      subject { project.find_current_release_mr("develop", "release") }
+      it { should be_nil }
+    end
+  end
 end
