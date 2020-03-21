3f6117398fd18389cc54ca82970b15ac36f1e4c3
sue445 <sue445@sue445.net>
2020-03-21 22:07:12 +0900

    Remove pry
    * Failed: https://github.com/sue445/gitlab_mr_release/pull/45/checks?check_run_id=523983993
    * Successful: https://github.com/sue445/gitlab_mr_release/runs/521565810?check_suite_focus=true
    
    pry is needless on ruby 2.7+. So I removed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,6 @@ require "webmock"
 require "dotenv"
 require "webmock/rspec"
 require "active_support/all"
-require "pry"
 require "rspec-parameterized"
 
 Dir["#{__dir__}/support/**/*.rb"].each { |f| require f }
